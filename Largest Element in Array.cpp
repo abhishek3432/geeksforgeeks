7a3c9a90cda1873b1d6c76b2923477000fe9f562
Abhishek Tiwari <akt343243@gmail.com>
2023-12-19 10:02:05 +0530

    commit

---
+++ Largest Element in Array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        //using for loop
+    int max=0;
+    for(int i=0; i<=n-2; i++)
+    {
+        if(arr[i+1]>=arr[max])
+            max=i+1;
+       
+    }
+    
+    return arr[max];
+        }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
