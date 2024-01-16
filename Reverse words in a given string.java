6a5c596cb2144cf8bcd3025ad7461c3b0c878392
Abhishek Tiwari <akt343243@gmail.com>
2024-01-16 21:36:24 +0530

    commit

---
+++ Reverse words in a given string.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    class GFG {
+        public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while (t > 0) {
+                String s = sc.next();
+                Solution obj = new Solution();
+                System.out.println(obj.reverseWords(s));
+                t--;
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    
+    class Solution 
+    {
+        //Function to reverse words in a given string.
+        String reverseWords(String S)
+        {
+            // code here
+            String s="";
+            String[] arr=S.split("\\.");
+            for(int i=arr.length-1;i>=0;i--){
+                if(i==0){
+                s=s+arr[i];
+                }else {
+                   s=s+arr[i]+"."; 
+                }
+            }
+            return s;
+        }
+    }
