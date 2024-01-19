80906833437d9714a5d91d0a4ff41cfb941be15f
Abhishek Tiwari <akt343243@gmail.com>
2024-01-19 22:47:49 +0530

    commit

---
+++ Pascal Triangle.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            ArrayList<Long> ans = new Solution().nthRowOfPascalTriangle(n);
+            printAns(ans);
+        }
+    }
+    public static void printAns(ArrayList<Long> ans) {
+        for (Long x : ans) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        // code here
+        ArrayList<Long> list = new ArrayList<>();
+        long ans = 1;
+        long mod = 1000000007;
+        list.add(ans);
+        
+        for(int col = 1; col < n; col++)
+        {
+            ans = ans * (n - col);
+            ans = ans / col;
+            list.add(ans % mod);
+        }
+        return list;
+    }
+    
+}
