8adaddc968a29873f5931ba1de0fd9d688c5b822
Abhishek Tiwari <akt343243@gmail.com>
2024-02-04 12:02:53 +0530

    commit

---
+++ Implement stack using array.java
@@ -0,0 +1,30 @@
+class MyStack
+{
+    int top;
+	int arr[] = new int[1000];
+
+    MyStack()
+	{
+		top = -1;
+	}
+	
+	//Function to push an integer into the stack.
+    void push(int a)
+	{
+	    // Your code here
+	    top++;
+	    arr[top] = a;
+	} 
+	
+    //Function to remove an item from top of the stack.
+	int pop()
+	{
+        // Your code here
+        //MUST CHECK STACK IS EMPTY OR NOT TO AVOID ERROR
+        if(top==-1) return -1;
+        int ans= arr[top];
+        
+        top--;
+        return ans;
+	}
+}
