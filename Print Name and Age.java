8092266681bc32531a87b2a1d713fd8f1fab14e3
Abhishek Tiwari <113165789+Abhishekgithubid@users.noreply.github.com>
2024-02-15 18:31:26 +0530

    Create Print Name and Age.java

---
+++ Print Name and Age.java
@@ -0,0 +1,32 @@
+import java.util.* ;
+
+import java.io.*; 
+
+import java.util.Scanner;
+
+ 
+
+class Solution {
+
+    
+
+    public static void main(String args[]) {
+
+        
+
+        // Write your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        int age = sc.nextInt();
+
+ 
+
+        System.out.println("My name is "+name+" "+"and my age is "+age+".");
+
+        
+
+    }
+}
