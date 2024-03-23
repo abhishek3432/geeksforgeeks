0520ec274491602466fe492336dff3be4309646b
Abhishek Tiwari <113165789+Abhishekgithubid@users.noreply.github.com>
2024-03-23 22:17:28 +0530

    Create logical Operators - Java.java

---
+++ logical Operators - Java.java
@@ -0,0 +1,10 @@
+class Geeks{
+    
+    static void logicOp(boolean a, boolean b){
+        /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/
+        System.out.print((a&&b)+" ");
+        System.out.print((a||b)+" ");
+        System.out.print((!a)&&(!b));
+        
+    }
+}
