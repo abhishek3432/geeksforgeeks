6494f989d58628bfe1dd2810874851d4a3f27b14
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-16 23:32:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a linked list/reverse-a-linked-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        // code here
+         //PointerApproach: take three pointer adjacent to each other and changing current node next pointer
+        //TC:(n)| SC:(1)
+        if(head==null)
+            return head;
+        Node pre = null;
+        Node cur = head;
+        Node nxt = head.next;
+
+        while(cur!=null)
+        {
+            cur.next=pre;
+            pre=cur;
+            cur=nxt;
+            if(nxt!=null) 
+                nxt=nxt.next;
+        }
+        return pre;
+    }
+}
